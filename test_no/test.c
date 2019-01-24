fd41b5b5920210a47b18a0ddce9157e312259eed
DrRyo <DrRyo@users.noreply.github.com>
2019-01-24 17:11:01 +0900

    Initialize repo

---
+++ test_no/test.c
@@ -0,0 +1,40 @@
+/*
+ * test.c
+ *
+ * Created: 2019-01-07 ¿ÀÈÄ 2:36:10
+ * Author: ryo
+ */
+
+#include <mega128.h>
+#include <delay.h>
+#include <stdio.h>
+
+void main(void)
+{
+    //unsigned int i = 2, n = 3;
+    //DDRG = 0;    
+    while (1)
+    {
+        //printf("%d, %d", DDG2, DDG3);
+        if(PING & 0x04){
+            if(PING & 0x08){
+                PORTG = 0x00;
+            }else{
+                PORTG = 0x02;
+            }
+        }else{
+            if(PING & 0x08){
+                PORTG = 0x01;
+            }else{
+                PORTG = 0x03;
+            }
+        }
+        delay_ms(200);
+    /*
+        PORTG = 0x03;
+        delay_ms(200);
+        PORTG = 0x00;
+        delay_ms(200);
+    */
+    }
+}
